0911ba9e8e5ad71891877d70e6320b951b733c0c
Danil <danil@vast.com>
2017-09-08 00:41:25 -0400

    RED: trivial rotation

--- src/test/java/com/vocumsineratio/kata/mars/RoverSimulationTest.java
+++ src/test/java/com/vocumsineratio/kata/mars/RoverSimulationTest.java
@@ -20,4 +20,14 @@ public class RoverSimulationTest {
         String finalState = TestableCore.simulateRover(initialState, trivialInstructions);
         Assert.assertEquals(finalState, initialState);
     }
+
+    @Test
+    public void testRotation () {
+        String initialState = "1 2 N";
+        String trivialInstructions = "L";
+
+        String finalState = TestableCore.simulateRover(initialState, trivialInstructions);
+        Assert.assertEquals(finalState, "1 2 W");
+
+    }
 }
