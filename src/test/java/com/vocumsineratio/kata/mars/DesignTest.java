0208a9bbb4cb1061371b8ba485ce6c02fc22f67d
Danil <danil@vast.com>
2017-09-22 22:20:29 -0400

    GREEN: introducing a test harness with less friction

---
+++ src/test/java/com/vocumsineratio/kata/mars/DesignTest.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright Vast 2018. All Rights Reserved.
+ * <p/>
+ * http://www.vast.com
+ */
+package com.vocumsineratio.kata.mars;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.nio.charset.Charset;
+
+/**
+ * @author Danil Suits (danil@vast.com)
+ */
+public class DesignTest {
+    @Test
+    public void testSampleProgram() {
+        check("5 5\n1 2 N\nLMLMLMLMM\n3 3 E\nMMRMMRMRRM\n", "1 3 N\n5 1 E\n");
+    }
+
+    private void check(String input, String expectedOutput) {
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        TestableCore.runTest(in, new PrintStream(baos));
+
+        byte [] actual = baos.toByteArray();
+        Assert.assertEquals(actual, expectedOutput.getBytes());
+    }
+}
