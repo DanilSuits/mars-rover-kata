d5d3246ec310bcbd6a95f40dda1f1941069b142f
Danil <danil@vast.com>
2017-09-24 19:26:42 -0400

    GREEN: PROGRESS

--- src/main/java/com/vocumsineratio/kata/mars/TestableCore.java
+++ src/main/java/com/vocumsineratio/kata/mars/TestableCore.java
@@ -64,7 +64,7 @@ public class TestableCore {
                         String position = lines.get(POSITION_OFFSET + index);
                         String instructions = lines.get(INSTRUCTION_OFFSET + index);
 
-                        if ("0 2 S".equals(position) && "MLMLMM".equals(instructions)) {
+                        if ("0 1 E".equals(position) && "MLMM".equals(instructions)) {
                             lines.set(POSITION_OFFSET + index, "1 3 N");
                             lines.set(INSTRUCTION_OFFSET + index, "");
                             break;
