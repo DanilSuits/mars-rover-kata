6ec0af7e4a43c48b9ff45a3b510e247c0af3fb61
Danil <danil@vast.com>
2017-09-24 15:01:08 -0400

    RED: failing test for S.left -> E

--- src/test/java/com/vocumsineratio/kata/mars/DesignTest.java
+++ src/test/java/com/vocumsineratio/kata/mars/DesignTest.java
@@ -40,7 +40,8 @@ public class DesignTest {
     Object[][] simplePrograms () {
         return new Object[][] {
                 {"1 2 N", "L", "1 2 W"},
-                {"1 2 W", "L", "1 2 S"}
+                {"1 2 W", "L", "1 2 S"},
+                {"1 2 S", "L", "1 2 E"}
         } ;
     }
     
