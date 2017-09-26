307553547aaea8db1882303d9067182b89860adc
Danil <danil@vast.com>
2017-09-26 13:19:28 -0400

    GREEN: reorganize model code

--- src/main/java/com/vocumsineratio/kata/mars/TestableCore.java
+++ src/main/java/com/vocumsineratio/kata/mars/TestableCore.java
@@ -22,100 +22,6 @@ import static java.lang.System.in;
  * @author Danil Suits (danil@vast.com)
  */
 public class TestableCore {
-    enum CompassPoint {
-        N, W, S, E
-    }
-
-    static final EnumMap<CompassPoint, CompassPoint> LEFT = new EnumMap<>(CompassPoint.class);
-    static {
-        LEFT.put(CompassPoint.N, CompassPoint.W);
-        LEFT.put(CompassPoint.W, CompassPoint.S);
-        LEFT.put(CompassPoint.S, CompassPoint.E);
-        LEFT.put(CompassPoint.E, CompassPoint.N);
-    }
-
-    static final EnumMap<CompassPoint, CompassPoint> RIGHT = new EnumMap<>(CompassPoint.class);
-    static {
-        RIGHT.put(CompassPoint.E, CompassPoint.S);
-        RIGHT.put(CompassPoint.S, CompassPoint.W);
-        RIGHT.put(CompassPoint.W, CompassPoint.N);
-        RIGHT.put(CompassPoint.N, CompassPoint.E);
-    }
-
-    static final EnumMap<CompassPoint, int []> MOVE = new EnumMap<>(CompassPoint.class);
-    static {
-        MOVE.put(CompassPoint.N, new int[]{0, 1});
-        MOVE.put(CompassPoint.S, new int[]{0, -1});
-        MOVE.put(CompassPoint.E, new int[]{1, 0});
-        MOVE.put(CompassPoint.W, new int[]{-1, 0});
-    }
-
-    static class Location {
-        int x;
-        int y;
-
-        public Location(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-
-    static class Position {
-        Location location;
-        CompassPoint heading;
-
-        Position(Location location, CompassPoint heading) {
-            this.location = location;
-            this.heading = heading;
-        }
-
-        void left() {
-            heading = LEFT.get(heading);
-        }
-
-        void right() {
-            heading = RIGHT.get(heading);
-        }
-
-        void move() {
-            int[] moves = MOVE.get(heading);
-            location.x += moves[0];
-            location.y += moves[1];
-        }
-    }
-
-    static class Rover {
-        Position position;
-        Program instructions;
-
-        Rover(Position position, Program instructions) {
-            this.position = position;
-            this.instructions = instructions;
-        }
-    }
-
-    static class Program {
-        String remainingInstructions;
-
-        Program(String remainingInstructions) {
-            this.remainingInstructions = remainingInstructions;
-        }
-
-        boolean hasCurrent() {
-            return ! remainingInstructions.isEmpty();
-        }
-
-        String current () {
-            return remainingInstructions.substring(FIRST_INSTRUCTION_OFFSET, NEXT_INSTRUCTION_OFFSET);
-        }
-
-        void next() {
-            remainingInstructions = remainingInstructions.substring(NEXT_INSTRUCTION_OFFSET);
-        }
-
-        static final int FIRST_INSTRUCTION_OFFSET = 0;
-        static final int NEXT_INSTRUCTION_OFFSET = 1;
-    }
 
     static class Domain {
         static class Squad implements API.Squad {
@@ -155,6 +61,101 @@ public class TestableCore {
                     }
                 }
             }
+        }
+
+        enum CompassPoint {
+            N, W, S, E
+        }
+
+        static final EnumMap<CompassPoint, CompassPoint> LEFT = new EnumMap<>(CompassPoint.class);
+        static {
+            LEFT.put(CompassPoint.N, CompassPoint.W);
+            LEFT.put(CompassPoint.W, CompassPoint.S);
+            LEFT.put(CompassPoint.S, CompassPoint.E);
+            LEFT.put(CompassPoint.E, CompassPoint.N);
+        }
+
+        static final EnumMap<CompassPoint, CompassPoint> RIGHT = new EnumMap<>(CompassPoint.class);
+        static {
+            RIGHT.put(CompassPoint.E, CompassPoint.S);
+            RIGHT.put(CompassPoint.S, CompassPoint.W);
+            RIGHT.put(CompassPoint.W, CompassPoint.N);
+            RIGHT.put(CompassPoint.N, CompassPoint.E);
+        }
+
+        static final EnumMap<CompassPoint, int []> MOVE = new EnumMap<>(CompassPoint.class);
+        static {
+            MOVE.put(CompassPoint.N, new int[]{0, 1});
+            MOVE.put(CompassPoint.S, new int[]{0, -1});
+            MOVE.put(CompassPoint.E, new int[]{1, 0});
+            MOVE.put(CompassPoint.W, new int[]{-1, 0});
+        }
+
+        static class Location {
+            int x;
+            int y;
+
+            public Location(int x, int y) {
+                this.x = x;
+                this.y = y;
+            }
+        }
+
+        static class Position {
+            Location location;
+            CompassPoint heading;
+
+            Position(Location location, CompassPoint heading) {
+                this.location = location;
+                this.heading = heading;
+            }
+
+            void left() {
+                heading = LEFT.get(heading);
+            }
+
+            void right() {
+                heading = RIGHT.get(heading);
+            }
+
+            void move() {
+                int[] moves = MOVE.get(heading);
+                location.x += moves[0];
+                location.y += moves[1];
+            }
+        }
+
+        static class Rover {
+            Position position;
+            Program instructions;
+
+            Rover(Position position, Program instructions) {
+                this.position = position;
+                this.instructions = instructions;
+            }
+        }
+
+        static class Program {
+            String remainingInstructions;
+
+            Program(String remainingInstructions) {
+                this.remainingInstructions = remainingInstructions;
+            }
+
+            boolean hasCurrent() {
+                return ! remainingInstructions.isEmpty();
+            }
+
+            String current () {
+                return remainingInstructions.substring(FIRST_INSTRUCTION_OFFSET, NEXT_INSTRUCTION_OFFSET);
+            }
+
+            void next() {
+                remainingInstructions = remainingInstructions.substring(NEXT_INSTRUCTION_OFFSET);
+            }
+
+            static final int FIRST_INSTRUCTION_OFFSET = 0;
+            static final int NEXT_INSTRUCTION_OFFSET = 1;
         }
     }
 
@@ -198,7 +199,7 @@ public class TestableCore {
         };
 
         static final class Squad extends Domain.Squad implements Lines.Model {
-            Squad(List<Rover> squad) {
+            Squad(List<Domain.Rover> squad) {
                 super(squad);
             }
 
@@ -214,29 +215,29 @@ public class TestableCore {
 
         static class Parser {
 
-            static final Function<String, Position> parsePosition = line -> {
+            static final Function<String, Domain.Position> parsePosition = line -> {
                 final String currentLocation = line.substring(0, line.length()-2);
                 String[] rawCoordinates = currentLocation.split(" ");
 
                 int xPos = Integer.parseInt(rawCoordinates[0]);
                 int yPos = Integer.parseInt(rawCoordinates[1]);
-                Location roverLocation = new Location(xPos, yPos);
+                Domain.Location roverLocation = new Domain.Location(xPos, yPos);
 
                 final String startHeading = line.substring(line.length() - 1);
-                CompassPoint currentHeading = CompassPoint.valueOf(startHeading);
+                Domain.CompassPoint currentHeading = Domain.CompassPoint.valueOf(startHeading);
 
-                return new Position(roverLocation, currentHeading);
+                return new Domain.Position(roverLocation, currentHeading);
             };
 
-            static Rover from(String rawPosition, String rawInstructions) {
-                return new Rover(
+            static Domain.Rover from(String rawPosition, String rawInstructions) {
+                return new Domain.Rover(
                         parsePosition.apply(rawPosition),
-                        new Program(rawInstructions)
+                        new Domain.Program(rawInstructions)
                 );
             }
 
-            static List<Rover> toSquad(List<String> lines) {
-                List<Rover> squad = new ArrayList<>();
+            static List<Domain.Rover> toSquad(List<String> lines) {
+                List<Domain.Rover> squad = new ArrayList<>();
 
                 int POSITION_OFFSET = 0;
                 int INSTRUCTION_OFFSET = 1;
