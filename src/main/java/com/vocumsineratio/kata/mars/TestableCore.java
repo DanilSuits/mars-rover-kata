680bcb16ad527cab29d132b6204c7a0ae0e78e56
Danil <danil@vast.com>
2017-11-03 00:02:56 -0400

    GREEN: isolate the output action

--- src/main/java/com/vocumsineratio/kata/mars/TestableCore.java
+++ src/main/java/com/vocumsineratio/kata/mars/TestableCore.java
@@ -7,15 +7,19 @@ package com.vocumsineratio.kata.mars;
 
 import java.io.InputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Danil Suits (danil@vast.com)
  */
 public class TestableCore {
     static void runTest(InputStream in, PrintStream out) {
-        // Simplest thing that can possibly work
-        out.println("1 3 N");
-        out.println("5 1 E");
+
+        String [] positions = { "1 3 N", "5 1 E"};
+        for(String position : positions) {
+            out.println(position);
+        }
     }
 
     public static void main(String[] args) {
