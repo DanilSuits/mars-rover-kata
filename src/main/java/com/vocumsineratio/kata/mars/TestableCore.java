d33b85db58ba710edee871b4246aeca0ae4a5a08
Danil <danil@vast.com>
2017-11-04 12:15:04 -0400

    GREEN: make the next change easy

--- src/main/java/com/vocumsineratio/kata/mars/TestableCore.java
+++ src/main/java/com/vocumsineratio/kata/mars/TestableCore.java
@@ -39,7 +39,12 @@ public class TestableCore {
 
             switch (input.get(4).substring(0,1)) {
                 case "R": {
-                    position[2] = "E";
+                    switch (position[2]) {
+                        case "N" : {
+                            position[2] = "E";
+                            break ;
+                        }
+                    }
                     break ;
                 }
 
