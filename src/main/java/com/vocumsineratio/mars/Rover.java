c63b8e27c253db383d0d02ffdebda9ae00c76e70
Danil <danil@vast.com>
2022-06-11 00:44:14 -0400

    GREEN: east is already implemented

--- src/main/java/com/vocumsineratio/mars/Rover.java
+++ src/main/java/com/vocumsineratio/mars/Rover.java
@@ -90,7 +90,7 @@ class Rover {
         State crntState = new State(4, 3, "E");
         {
             {
-                crntState = new State(5, 3, "E");
+                crntState = east(crntState);
             }
         }
         {
