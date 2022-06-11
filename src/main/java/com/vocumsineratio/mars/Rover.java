dea23fc1dd8f7de01412263adb5eedadb8805c53
Danil <danil@vast.com>
2022-06-10 22:54:36 -0400

    GREEN: use a function for formatting the output information

--- src/main/java/com/vocumsineratio/mars/Rover.java
+++ src/main/java/com/vocumsineratio/mars/Rover.java
@@ -4,6 +4,11 @@ package com.vocumsineratio.mars;
  * @author Danil Suits (danil@vast.com)
  */
 class Rover {
+    static class Schema {
+        static String rover(int x, int y, String orientation) {
+            return String.format("%d %d %s", x, y, orientation);
+        }
+    }
     static String [] output(String... lines) {
         assert lines[0].equals("5 5");
         assert lines[1].equals("1 2 N");
@@ -13,7 +18,7 @@ class Rover {
 
 
         String[] report = new String[2];
-        report[0] = "1 3 N";
+        report[0] = Schema.rover(1, 3, "N");
         report[1] = "5 1 E";
         return report;
     }
